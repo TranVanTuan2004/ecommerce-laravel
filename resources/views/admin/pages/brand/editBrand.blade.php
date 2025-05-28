33c9562fb9ca75cd5606fd910f9d55a209685c19
Tuấn Trần <tuantran280504@gmail.com>
2025-05-29 01:36:15 +0700

    câp nhật db

--- resources/views/admin/pages/brand/editBrand.blade.php
+++ resources/views/admin/pages/brand/editBrand.blade.php
@@ -33,7 +33,8 @@
                     </div>
                 @endif
 
-                <form method="POST" action="{{ route('brand.update', $brand->id) }}" enctype="multipart/form-data">
+                <form method="POST" id="brand-form" action="{{ route('brand.update', $brand->id) }}"
+                    enctype="multipart/form-data">
                     @csrf
                     @method('PUT')
                     <input type="hidden" name="updated_at" value="{{ $brand->updated_at }}">
@@ -62,10 +63,17 @@
 
                     <div class="d-flex justify-content-end mt-4">
                         <a href="{{ route('brand.index') }}" class="btn btn-secondary me-2">Quay lại</a>
-                        <button type="submit" class="btn btn-success">Cập nhật</button>
+                        <button type="submit" id="submit-btn" class="btn btn-success">Cập nhật</button>
                     </div>
                 </form>
             </div>
         </div>
     </div>
+    <script>
+        document.getElementById('brand-form').addEventListener('submit', function(e) {
+            const btn = document.getElementById('submit-btn');
+            btn.disabled = true;
+            btn.innerText = 'Đang xử lý...';
+        });
+    </script>
 @endsection
