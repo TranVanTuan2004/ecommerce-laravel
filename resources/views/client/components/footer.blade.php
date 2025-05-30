775681753d7e065a3dade9142678173089643382
haus0409 <haconghau07@gmail.com>
2025-05-30 12:39:03 +0700

    footer

--- resources/views/client/components/footer.blade.php
+++ resources/views/client/components/footer.blade.php
@@ -10,51 +10,57 @@
             </div>
 
             <!-- Footer Links -->
+            <!-- Column 1 -->
             <div class="col-lg-2 col-md-4 col-6 mb-4">
                 <h5 class="footer-heading mb-3">Home</h5>
                 <ul class="list-unstyled">
-                    <li><a href="#" class="text-decoration-none">Home</a></li>
-                    <li><a href="#" class="text-decoration-none">Elements</a></li>
+                    <li><a href="#" class="text-decoration-none text-white">Home</a></li>
+                    <li><a href="#" class="text-decoration-none text-white">Shop</a></li>
                 </ul>
             </div>
 
+            <!-- Column 2 -->
             <div class="col-lg-2 col-md-4 col-6 mb-4">
-                <h5 class="footer-heading mb-3">Elements</h5>
+                <h5 class="footer-heading mb-3">Categories</h5>
                 <ul class="list-unstyled">
-                    <li><a href="#" class="text-decoration-none">Elements</a></li>
-                    <li><a href="#" class="text-decoration-none">Elements</a></li>
+                    <li><a href="#" class="text-decoration-none text-white">Men</a></li>
+                    <li><a href="#" class="text-decoration-none text-white">Women</a></li>
                 </ul>
             </div>
 
+            <!-- Column 3 -->
             <div class="col-lg-2 col-md-4 col-6 mb-4">
-                <h5 class="footer-heading mb-3">Elements</h5>
+                <h5 class="footer-heading mb-3">Customer</h5>
                 <ul class="list-unstyled">
-                    <li><a href="#" class="text-decoration-none">Elements</a></li>
-                    <li><a href="#" class="text-decoration-none">Elements</a></li>
+                    <li><a href="#" class="text-decoration-none text-white">Help Center</a></li>
+                    <li><a href="#" class="text-decoration-none text-white">Returns</a></li>
                 </ul>
             </div>
 
+            <!-- Column 4 -->
             <div class="col-lg-2 col-md-4 col-6 mb-4">
-                <h5 class="footer-heading mb-3">Elements</h5>
+                <h5 class="footer-heading mb-3">Company</h5>
                 <ul class="list-unstyled">
-                    <li><a href="#" class="text-decoration-none">Elements</a></li>
-                    <li><a href="#" class="text-decoration-none">Elements</a></li>
+                    <li><a href="#" class="text-decoration-none text-white">About Us</a></li>
+                    <li><a href="#" class="text-decoration-none text-white">Careers</a></li>
                 </ul>
             </div>
 
+            <!-- Column 5 -->
             <div class="col-lg-2 col-md-4 col-6 mb-4">
-                <h5 class="footer-heading mb-3">Elements</h5>
+                <h5 class="footer-heading mb-3">Policies</h5>
                 <ul class="list-unstyled">
-                    <li><a href="#" class="text-decoration-none">Elements</a></li>
-                    <li><a href="#" class="text-decoration-none">Elements</a></li>
+                    <li><a href="#" class="text-decoration-none text-white">Privacy</a></li>
+                    <li><a href="#" class="text-decoration-none text-white">Terms</a></li>
                 </ul>
             </div>
 
+            <!-- Column 6 -->
             <div class="col-lg-2 col-md-4 col-6 mb-4">
-                <h5 class="footer-heading mb-3">Elements</h5>
+                <h5 class="footer-heading mb-3">Follow Us</h5>
                 <ul class="list-unstyled">
-                    <li><a href="#" class="text-decoration-none">Elements</a></li>
-                    <li><a href="#" class="text-decoration-none">Elements</a></li>
+                    <li><a href="#" class="text-decoration-none text-white">Instagram</a></li>
+                    <li><a href="#" class="text-decoration-none text-white">Facebook</a></li>
                 </ul>
             </div>
 
@@ -66,8 +72,8 @@
             <!-- Copyright -->
             <div class="col-12 text-center mt-4">
                 <p class="copyright">
-                    Copyright © 2025 <a href="#" class="text-white">XStore theme</a>. Created by 8theme - <a
-                        href="#" class="text-white">WordPress WooCommerce themes</a>
+                    Copyright © 2025 <a href="#" class="text-white">XStore theme</a>. Created by 8theme - <a href="#"
+                        class="text-white">WordPress WooCommerce themes</a>
                 </p>
             </div>
         </div>
